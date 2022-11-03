23ea16710fd88c9d711fb9ac1a611bcc04067cb3
nathanielzacharias <nathanielzacharias@gmail.com>
2022-11-03 16:07:08 +0800

    feat:added bare basic navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import { Routes, Route } from "react-router-dom";
+import TopNavbar from './components/TopNavbar';
 
 
 function App() {
   return (
     <div className="App">
+      <TopNavbar/>
       <h1>Hello Nat</h1>
       
     </div>
---
+++ src/components/TopNavbar.jsx
@@ -0,0 +1,9 @@
+function TopNavbar() {
+    return(
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <p>test</p>
+        </nav>
+    )
+}
+
+export default TopNavbar;
